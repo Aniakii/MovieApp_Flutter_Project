27c03db97c20f3c14d8b02de96c7b4ef4db478ef
Aniakii <95714597+Aniakii@users.noreply.github.com>
2023-09-29 17:48:18 +0200

    Delete constants directory

--- constants/color_palette.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color brighterPurple = Colors.purple;
-  static const Color darkerPurple = Color(0xFF401197);
-  static const Color grey = Color(0xff303030);
-
-  const AppColors._();
-}
