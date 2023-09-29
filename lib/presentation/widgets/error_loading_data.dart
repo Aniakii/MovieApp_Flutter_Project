9f12614bb091046b5de980280b6dd877ae42dca0
Karol Majka <karolmajka95@gmail.com>
2023-09-29 17:50:54 +0200

    Add new widgets

---
+++ lib/presentation/widgets/error_loading_data.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../../constants/color_palette.dart';
+import '../../constants/texts.dart';
+
+class ErrorLoadingDataWidget extends StatelessWidget {
+  const ErrorLoadingDataWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: AppColors.darkerPurple,
+      body: Center(
+        child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(Icons.error, color: AppColors.brighterPurple,
+                    size: 100.0,),
+                  SizedBox(height: 50.0),
+                  Text(LoadingTexts.failedText, style: TextStyle(fontSize: 30.0)),
+                ],
+              ),
+      ),
+    );
+  }
+}
