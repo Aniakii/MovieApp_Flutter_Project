ba47aa5584fa496a7dfcc270922337310e572468
Aniakii <95714597+Aniakii@users.noreply.github.com>
2023-09-29 17:43:37 +0200

    updated and refactored constant files

---
+++ constants/color_palette.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color brighterPurple = Colors.purple;
+  static const Color darkerPurple = Color(0xFF401197);
+  static const Color grey = Color(0xff303030);
+
+  const AppColors._();
+}
---
+++ constants/enums.dart
@@ -0,0 +1,17 @@
+enum SortingType {
+  fromAtoZ,
+  fromZtoA,
+  worstRate,
+  bestRate,
+  mostPopular,
+  leastPopular,
+  defaultSorting,
+}
+
+enum LoadingDataStatus {
+  loading,
+  errorOnline,
+  errorOffline,
+  errorOther,
+  ready,
+}
