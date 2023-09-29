1ae3c5f372aaeb032f7e79b65570a3228dd3b6e2
Aniakii <95714597+Aniakii@users.noreply.github.com>
2023-09-29 17:49:55 +0200

    Delete lib/constants/alerts.dart

--- lib/constants/alerts.dart
+++
@@ -1,7 +0,0 @@
-const String onlineAlertTitle = "NO INTERNET CONNECTION";
-const String onlineAlertDesc = "It seems that you don't have an internet connection to download movie data. Do you want to try again loading data or show data from yours device memory (offline mode)?";
-const String tryAgainButton = "TRY AGAIN";
-const String goOfflineButton = "GO OFFLINE";
-
-const String offlineAlertTitle = "NO DATA IN DEVICES MEMORY";
-const String offlineAlertDesc = "It seems that you haven't used the app yet and there is no data saved on your device. Close the app and open it when you have internet connection.";
