74c0f859966633a9c1ff381fadb63215b83f4f81
Ania Majka <aniamajka0801@gmail.com>
2023-09-30 22:06:36 +0200

    Improved header widget

--- lib/presentation/widgets/header.dart
+++ lib/presentation/widgets/header.dart
@@ -10,21 +10,27 @@ class Header extends StatelessWidget {
     return const Padding(
       padding: EdgeInsets.all(15.0),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            AppTexts.headerTitle,
-            style: TextStyle(
-                fontSize: 25.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.white),
+          FittedBox(
+            fit: BoxFit.scaleDown,
+            child: Text(
+              AppTexts.headerTitle,
+              style: TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
           ),
           SizedBox(
             height: 5.0,
           ),
-          Text(
-            AppTexts.headerDesc,
-            style: TextStyle(fontSize: 18.0, color: Colors.white),
+          FittedBox(
+            fit: BoxFit.scaleDown,
+            child: Text(
+              AppTexts.headerDesc,
+              style: TextStyle(fontSize: 18.0, color: Colors.white),
+            ),
           ),
         ],
       ),
