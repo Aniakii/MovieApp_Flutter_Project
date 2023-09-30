4664ed9d8a7ba303e06dce34424eefb451455f19
Ania Majka <aniamajka0801@gmail.com>
2023-09-30 13:54:38 +0200

    Deleted unnecessary functions

--- lib/data/models/genre.dart
+++ lib/data/models/genre.dart
@@ -12,18 +12,4 @@ class Genre {
   final String name;
 
   Genre({required this.id, required this.name});
-
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'name': name,
-    };
-  }
-
-  static Genre fromJson(Map<String, dynamic> json) {
-    return Genre(
-      id: json['id'],
-      name: json['name'],
-    );
-  }
 }
